9fa7ec277c5234c10f613cfdc2a5200551ff3143
Pawel Piatek <pawelpiatek98@gmail.com>
2020-05-13 16:47:44 +0200

    refactored form

--- assets/variables.js
+++ assets/variables.js
@@ -2,4 +2,4 @@
 export const white = '#fff';
 export const primaryGrey = 'rgba(51, 51, 51, 1)';
 export const primaryYellow = '#ffc845';
-
+export const red = '#f85a40';
--- components/orderForm.js
+++ components/orderForm.js
@@ -18,7 +18,11 @@ padding: 30px;
 
 
 const Error = styled.Text`
-
+  font-size: 10px;
+  color: ${variables.red};
+  margin: 0 auto;
+  text-align: center;
+  width: 100%;
 `;
 
 const onSubmit = (values) => {
@@ -29,21 +33,65 @@ const onSubmit = (values) => {
 export default function OrderForm() {
     return (
         <Formik
-            initialValues={{ email: '', password: '' }}
+            initialValues={{
+                email: '',
+                name: '',
+                lastName: '',
+                phone: '',
+                city: '',
+                street: '',
+                house: ''
+            }}
             onSubmit={values => Alert.alert(JSON.stringify(values))}
             validationSchema={yup.object().shape({
                 email: yup
                     .string()
                     .email()
                     .required(),
-                password: yup
+                name: yup
                     .string()
-                    .min(6)
+                    .required(),
+                lastName: yup
+                    .string()
+                    .required(),
+                phone: yup
+                    .number()
+                    .required(),
+                city: yup
+                    .string()
+                    .required(),
+                street: yup
+                    .string()
+                    .required(),
+                house: yup
+                    .string()
                     .required(),
             })}
         >
             {({ values, handleChange, errors, setFieldTouched, touched, isValid, handleSubmit }) => (
                 <FormWrapper>
+                    <TextInput
+                        style={styles.input}
+                        value={values.name}
+                        onChangeText={handleChange('name')}
+                        placeholder="Name"
+                        onBlur={() => setFieldTouched('name')}
+
+                    />
+                    {touched.name && errors.name &&
+                    <Error>{errors.name}</Error>
+                    }
+                    <TextInput
+                        style={styles.input}
+                        value={values.lastName}
+                        onChangeText={handleChange('lastName')}
+                        placeholder="Last name"
+                        onBlur={() => setFieldTouched('lastName')}
+
+                    />
+                    {touched.lastName && errors.lastName &&
+                    <Error>{errors.lastName}</Error>
+                    }
                     <TextInput
                         style={styles.input}
                         value={values.email}
@@ -52,21 +100,55 @@ export default function OrderForm() {
                         placeholder="E-mail"
                     />
                     {touched.email && errors.email &&
-                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
+                    <Error>{errors.email}</Error>
+                    }
+                    <TextInput
+                        keyboardType='numeric'
+                        style={styles.input}
+                        value={values.phone}
+                        onChangeText={handleChange('phone')}
+                        placeholder="Phone"
+                        onBlur={() => setFieldTouched('phone')}
+
+                    />
+                    {touched.phone && errors.phone &&
+                    <Error>{errors.phone}</Error>
                     }
                     <TextInput
                         style={styles.input}
-                        value={values.password}
-                        onChangeText={handleChange('password')}
-                        placeholder="Password"
-                        onBlur={() => setFieldTouched('password')}
-                        secureTextEntry={true}
+                        value={values.city}
+                        onChangeText={handleChange('city')}
+                        placeholder="City"
+                        onBlur={() => setFieldTouched('city')}
+
                     />
-                    {touched.password && errors.password &&
-                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
+                    {touched.city && errors.city &&
+                    <Error>{errors.city}</Error>
+                    }
+                    <TextInput
+                        style={styles.input}
+                        value={values.street}
+                        onChangeText={handleChange('street')}
+                        placeholder="Street"
+                        onBlur={() => setFieldTouched('street')}
+
+                    />
+                    {touched.street && errors.street &&
+                    <Error>{errors.street}</Error>
+                    }
+                    <TextInput
+                        style={styles.input}
+                        value={values.house}
+                        onChangeText={handleChange('house')}
+                        placeholder="House"
+                        onBlur={() => setFieldTouched('house')}
+
+                    />
+                    {touched.house && errors.house &&
+                    <Error>{errors.house}</Error>
                     }
                     <Button
-                        title='Sign In'
+                        title='Order'
                         disabled={!isValid}
                         onPress={handleSubmit}
                     />
